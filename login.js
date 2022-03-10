91956504c77ce54150b43105371105499a000299
eunjungL <dldmswjd322@gmail.com>
2022-03-10 15:51:36 +0900

    [기능추가]로그인 오류(아이디/비밀번호 오류) 방지 코드 추가

--- login.js
+++ login.js
@@ -11,12 +11,25 @@ app.post('/', async (req, res) => {
     const id = body.id;
     const password = body.password;
 
-    const [user, field] = await db.execute(`SELECT * FROM admin_user WHERE admin_id = ?`, [id]);
-    const encode_pwd = await bcrypt.compare(password, user[0].password);
-    if (encode_pwd) {
-        const token = await jwt.sign({id: user[0].admin_id, nickname: user[0].nickname}, 'secret', {expiresIn: '7d'});
-        res.send({token: token});
+    if (id === undefined || password === undefined) {
+        res.send({token: 'false'});
+    } else {
+        const [user, field] = await db.execute(`SELECT * FROM admin_user WHERE admin_id = ?`, [id]);
+
+        if (user.length === 0) {
+            res.send({token: 'id_false'});
+        } else {
+            const encode_pwd = await bcrypt.compare(password, user[0].password);
+
+            if (encode_pwd) {
+                const token = await jwt.sign({id: user[0].admin_id, nickname: user[0].nickname}, 'secret', {expiresIn: '7d'});
+                res.send({token: token});
+            } else {
+                res.send({token: 'pwd_false'});
+            }
+        }
     }
+
 })
 
 module.exports = app;
