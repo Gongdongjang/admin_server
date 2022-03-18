598440e485e841e9bcac96a1e7ddb6dd31a15669
eunjungL <dldmswjd322@gmail.com>
2022-03-19 02:26:41 +0900

    [기능변경] 쿠키용 미들웨어로 전환 & 로그인 url pass 추가

--- auth_middleware.js
+++ auth_middleware.js
@@ -1,19 +1,32 @@
 const jwt = require('jsonwebtoken');
 const jwt_secret = require('./config').jwt_secret;
 
+const get_cookies = (req) => {
+    let cookies = {};
+    req.headers && req.headers.cookie.split(';').forEach(function(cookie) {
+        let parts = cookie.match(/(.*?)=(.*)$/)
+        cookies[ parts[1].trim() ] = (parts[2] || '').trim();
+    });
+    return cookies;
+};
+
 const auth_middleware = async (req, res, next) => {
-    let access_token = req.headers.authorization;
-
-    if (access_token === undefined) { // access_token 없으면 로그인 X 상태
-        res.status(401).send({msg: "unauthorized"});
+    if (req.headers.referer.indexOf('/login')) {
+        console.log('login pass');
+        next();
     } else {
-        access_token = access_token.split('Bearer ')[1];
+        let access_token = get_cookies(req)['access_token'];
+        console.log(access_token);
 
-        try { // access_token 유효하면 인증 완료
-            req.decode = jwt.verify(access_token, jwt_secret);
-            next();
-        } catch (e) { // access_token 만료 -> /refresh 필요
+        if (access_token === undefined) { // access_token 없으면 로그인 X 상태
             res.status(401).send({msg: "unauthorized"});
+        } else {
+            try { // access_token 유효하면 인증 완료
+                req.decode = jwt.verify(access_token, jwt_secret);
+                next();
+            } catch (e) { // access_token 만료 -> /refresh 필요
+                res.status(401).send({msg: "unauthorized"});
+            }
         }
     }
 }
