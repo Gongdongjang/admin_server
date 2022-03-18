19e57c6965f5271d321b5ce075c8e2e0d0bd0245
eunjungL <dldmswjd322@gmail.com>
2022-03-19 03:44:54 +0900

    [기능변경] refresh token도 pass 되게 변경

--- auth_middleware.js
+++ auth_middleware.js
@@ -14,17 +14,17 @@ const get_cookies = (req) => {
 };
 
 const auth_middleware = async (req, res, next) => {
-    if (req.headers.referer.indexOf('/login') !== -1) {
+    if (req.originalUrl.indexOf('/login') !== -1) {
         console.log('login pass');
         next();
     } else {
-        let access_token = get_cookies(req);
+        let token = get_cookies(req);
+        const access_token = token['access_token'];
         console.log(access_token);
-
         if (access_token === undefined) { // access_token 없으면 로그인 X 상태
             res.status(401).send({msg: "unauthorized"});
         } else {
-            access_token = access_token['access_token'];
+            // access_token = access_token['access_token'];
             try { // access_token 유효하면 인증 완료
                 req.decode = jwt.verify(access_token, jwt_secret);
                 console.log(req.decode);
