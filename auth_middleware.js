dbdb0bba8244b6185fe0d168822c2f4eaf0464f9
eunjungL <dldmswjd322@gmail.com>
2022-03-19 14:27:31 +0900

    [기능변경]auth_middleware에 토큰이 아예 없는 경우 오류 보내기 추가

--- auth_middleware.js
+++ auth_middleware.js
@@ -14,23 +14,25 @@ const get_cookies = (req) => {
 };
 
 const auth_middleware = async (req, res, next) => {
-    if (req.originalUrl.indexOf('/login') !== -1) {
+    if (req.originalUrl.indexOf('/login') !== -1) { // 로그인 관련 시도는 auth_middleware 통과하게
         console.log('login pass');
         next();
     } else {
         let token = get_cookies(req);
-        const access_token = token['access_token'];
-        console.log(access_token);
-        if (access_token === undefined) { // access_token 없으면 로그인 X 상태
+        if (token === undefined) {
             res.status(401).send({msg: "unauthorized"});
         } else {
-            // access_token = access_token['access_token'];
-            try { // access_token 유효하면 인증 완료
-                req.decode = jwt.verify(access_token, jwt_secret);
-                console.log(req.decode);
-                next();
-            } catch (e) { // access_token 만료 -> /refresh 필요
+            const access_token = token['access_token'];
+            if (access_token === undefined) { // access_token 없으면 로그인 X 상태
                 res.status(401).send({msg: "unauthorized"});
+            } else {
+                // access_token = access_token['access_token'];
+                try { // access_token 유효하면 인증 완료
+                    req.decode = jwt.verify(access_token, jwt_secret);
+                    next();
+                } catch (e) { // access_token 만료 -> /refresh 필요
+                    res.status(401).send({msg: "unauthorized"});
+                }
             }
         }
     }
