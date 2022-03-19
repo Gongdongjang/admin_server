cf7a238e06466bb8f3962ae21d67f0b2c1ca75ec
eunjungL <dldmswjd322@gmail.com>
2022-03-19 15:00:40 +0900

    [기능추가] 사용자 인증을 위한 auth_middleware 추가

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const login_register = require('./login');
 const cors = require('cors');
+const auth_middleware = require('./auth_middleware');
 
 app.use(cors());
+app.use(auth_middleware);
 app.use('/api/login', login_register);
 
-app.get('/', async (req, res) => {
-    res.send({test: 'initial commit'});
+app.get('/api', async (req, res) => {
+    res.send(req.decode);
 })
 
 app.listen(5000);
