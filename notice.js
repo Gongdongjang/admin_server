9472d632a6d17d5cbacfa0f029d51b7709c55e25
eunjungL <dldmswjd322@gmail.com>
2022-05-05 00:07:35 +0900

    [기능추가] 공지사항 삭제 기능 추가

--- notice.js
+++ notice.js
@@ -47,4 +47,32 @@ app.post('/', upload.single('photo'), async (req, res) => {
     }
 })
 
+// notice 삭제
+app.delete('/delete/:notice_id', async (req, res) => {
+    const notice_id = req.params.notice_id;
+
+    try {
+        const [notice, field] = await db.execute(`SELECT * FROM notice WHERE notice_id = ?`, [notice_id]);
+        const photo = notice[0].notice_photo;
+
+        const [result] = await db.execute(`DELETE FROM notice WHERE notice_id = ?`, [notice_id]);
+        // 삭제 성공하면 photo 도 s3 에서 삭제
+        if (result) {
+            if (photo) {
+                s3.deleteObject({
+                    Bucket: 'gdjang',
+                    Key: photo
+                }, (err, data) => {
+                    if (err) console.log(err);
+                    else console.log(data);
+                });
+            }
+            res.send({notice_id: notice_id, msg: '삭제 성공'});
+        } else res.status(400).send({msg: '삭제 실패'});
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({msg: 'server error'});
+    }
+})
+
 module.exports = app;
