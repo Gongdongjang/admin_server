f9a6c10eb71e1e5e7d8054ff727339021c53583e
eunjungL <dldmswjd322@gmail.com>
2022-05-10 15:38:31 +0900

    [기능추가] 공지사항 라우터 추가

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const login_register = require('./login');
+const notice_register = require('./notice');
 const cors = require('cors');
 const auth_middleware = require('./auth_middleware');
 
 app.use(cors());
 app.use(auth_middleware);
 app.use('/api/login', login_register);
+app.use('/api/notice', notice_register);
 
 app.get('/api', async (req, res) => {
     res.send(req.decode);
