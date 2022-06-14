1788b229a56adc1ebcdbe4e4a6d8e1fdaa4c341c
eunjungL <dldmswjd322@gmail.com>
2022-06-15 01:00:09 +0900

    [기능추가] 사장님 회원가입 전화번호  문자 인증 기능 추가

--- signup.js
+++ signup.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
 const db = require('./db');
+const axios = require('axios');
+const CryptoJS = require('crypto-js');
+const naver = require('./config').naver;
 
 app.use(express.json());
 
@@ -36,6 +39,98 @@ app.post('/id-check', async (req, res) => {
         if (result.length !== 0) is_valid = false;
         res.send({ is_valid: is_valid });
     } catch (e) {
+        res.status(500).send({ msg: 'server error' });
+    }
+})
+
+// sms 인증
+function makeSignature(time) {
+    var space = " ";				// one space
+    var newLine = "\n";				// new line
+    var method = "POST";				// method
+    var url = `/sms/v2/services/${naver.id}/messages`;	// url (include query string)
+    var timestamp = time;			// current timestamp (epoch)
+    var accessKey = naver.access;			// access key id (from portal or Sub Account)
+    var secretKey = naver.console_secret;			// secret key (from portal or Sub Account)
+
+    var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+    hmac.update(method);
+    hmac.update(space);
+    hmac.update(url);
+    hmac.update(newLine);
+    hmac.update(timestamp);
+    hmac.update(newLine);
+    hmac.update(accessKey);
+
+    var hash = hmac.finalize();
+
+    return hash.toString(CryptoJS.enc.Base64);
+}
+
+app.post('/phone-check', async (req, res) => {
+    const body = req.body;
+    const phone_number = body.phone_number;
+    console.log(body);
+
+    const sms_url = `https://sens.apigw.ntruss.com/sms/v2/services/${naver.id}/messages`;
+    const time_stamp = Date.now().toString();
+    const signature = makeSignature(time_stamp);
+    let code = '';
+    for (let i = 0; i < 6; i++) code += Math.floor(Math.random() * 10);
+    try {
+        // const sms_res = await axios.post(sms_url, {
+        //         "type":"SMS",
+        //         "from":"01099263238",
+        //         "countryCode": "82",
+        //         "content":`공동장 인증번호는 [${code}]입니다.`,
+        //         "messages":[
+        //             {
+        //                 "to": phone_number,
+        //                 "content":`공동장 인증번호는 [${code}]입니다.`
+        //             }
+        //         ]
+        //     }, {
+        //         headers: {
+        //             'Content-Type': 'application/json; charset=utf-8',
+        //             'x-ncp-apigw-timestamp': time_stamp,
+        //             'x-ncp-iam-access-key': naver.access,
+        //             'x-ncp-apigw-signature-v2': signature
+        //         }
+        //     }
+        // )
+        const [result] = await db.execute(`INSERT INTO sms_validation(phone_number, code, expire)
+                                           VALUES (?, ?, NOW() + INTERVAL 3 MINUTE) ON DUPLICATE KEY
+                UPDATE code = ?, expire = NOW() + INTERVAL 3 MINUTE`,
+            [phone_number, code, code]);
+        res.send({  msg: 'success'  });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({ msg: 'server error' });
+    }
+})
+
+app.post('/phone-check/verify', async (req, res) => {
+    const body = req.body;
+    const code = body.code;
+    const phone_number = body.phone_number;
+
+    let phone_valid = false;
+
+    console.log(code);
+    try {
+        const [result, field] = await db.execute(`SELECT *
+                                                  FROM sms_validation
+                                                  WHERE phone_number = ?`, [phone_number]);
+        const expire_time = new Date(result[0].expire).setHours(result[0].expire.getHours() + 9);
+        const now = Date.now();
+        console.log(expire_time, now)
+        if (code === result[0].code && expire_time > now) {
+            phone_valid = true;
+        }
+
+        res.send({phone_valid: phone_valid});
+    } catch (e) {
+        console.log(e);
         res.status(500).send({ msg: 'server error' });
     }
 })
