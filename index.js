78d9adfd8cec096bc9da3cd453709ee007a60ca5
eunjungL <dldmswjd322@gmail.com>
2023-02-06 18:13:14 +0900

    [기능추가] 알림 기능을 위한 notification.js 추가

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const login_register = require('./login');
+const notificationRegister = require('./notification');
 const cors = require('cors');
 const auth_middleware = require('./auth_middleware');
 
 app.use(cors());
 app.use(auth_middleware);
 app.use('/api/login', login_register);
+app.use('/api/notification', notificationRegister);
 
 app.get('/api', async (req, res) => {
     res.send(req.decode);
---
+++ notification.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const aws_key = require('./config').aws_access;
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multer_s3 = require('multer-s3');
+const db = require('./db');
+const firebase = require('firebase-admin')
+
+module.exports = app;
