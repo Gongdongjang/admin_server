75c201fd2dcb75b7e72ab69f327bdef7bc4b625f
eunjungL <62099953+eunjungL@users.noreply.github.com>
2023-02-24 00:34:32 +0900

    Revert "테스트용 로그인 기능 완성"

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
-const login_register = require('./login');
-const cors = require('cors');
-const auth_middleware = require('./auth_middleware');
 
-app.use(cors());
-app.use(auth_middleware);
-app.use('/api/login', login_register);
-
-app.get('/api', async (req, res) => {
-    res.send(req.decode);
+app.get('/', async (req, res) => {
+    res.send('initial commit');
 })
 
-app.listen(5000);
+app.listen(3000);
